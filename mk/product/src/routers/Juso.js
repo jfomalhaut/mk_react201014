7a845c927d7980fbf4fa1ec1a193ac79499906ad
min0916 <vovo0916@naver.com>
2020-11-16 20:22:52 +0900

    asd

--- mk/product/src/routers/Juso.js
+++ mk/product/src/routers/Juso.js
@@ -1,44 +1,85 @@
-import React from 'react';
-import Axios from 'axios';
+import React, { useState } from "react";
+import Axios from "axios";
 
 const JUSO_API = process.env.JUSO_API;
 const JUSO_URL = process.env.JUSO_URL;
 
 const Juso = () => {
-    
-    
-    console.log(JUSO_API);
-    console.log(JUSO_URL);
-    
-    const params = arr => {
-        const data =[];
-        for(let key in arr) {
-            data.push(`${key}=${arr[key]}`);
-        }
-        return data.join('&')
+  const [keyword, setKeyword] = useState("");
+  const [total, setTotal] = useState(0);
+  const [jusoList, setJuso] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const params = (arr) => {
+    const data = [];
+    for (let key in arr) {
+      data.push(`${key}=${arr[key]}`);
     }
+    return data.join("&");
+  };
 
-    const getJuso = () => {
-        const _payload = {
-            confmKey: JUSO_API,
-            currentPage: 1,
-            countPerPage: 10,
-            keyword: '역삼동',
-            resultType: 'json'
-        };
+  const getJuso = (ev) => {
+    ev.preventDefault();
+    search();
+  };
 
-        const payload = params(_payload);
-        console.log(payload)
-        Axios.get(`${JUSO_URL}?${payload}`).than(res => {
-            console.log(res);
-        }).catch(error => {
-            console.log(error);
-        })
+  const search = () => {
+    const _payload = {
+      confmKey: JUSO_API,
+      currentPage,
+      countPerPage: 10,
+      keyword,
+      resultType: "json",
     };
 
-    return (
-        <h1 onClick={getJuso}>Juso Component</h1>
-    )
-}
+    const payload = params(_payload);
+    console.log(payload);
+    Axios.get(`${JUSO_URL}?${payload}`)
+      .then((res) => {
+        const {
+          data: {
+            results: {
+              common: { totalCount },
+              juso,
+            },
+          },
+        } = res;
+        setJuso(juso);
+        setTotal(totalCount);
+        // console.log(juso);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const nextPage = () => {
+    const newState = 1;
+    console.log(newState);
+    setCurrentPage(newState+1);
+    search();
+  };
+
+  const backPage = () => {
+    setCurrentPage(currentPage - 1);
+    console.log(currentPage);
+    search();
+  };
+
+  return (
+    <form onSubmit={getJuso}>
+      <input value={keyword} onChange={(ev) => setKeyword(ev.target.value)} />
+      <button>검색</button>
+      <h1>총 {total}개의 검색결과</h1>
+      <ul>
+        {jusoList.map((item) => (
+          <li>{item.roadAddrPart1}</li>
+        ))}
+      </ul>
+      <button onClick={backPage}>이전</button>
+      <button onClick={nextPage}>다음</button>
+    </form>
+  );
+};
 
 export default Juso;
